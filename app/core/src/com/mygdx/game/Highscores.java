b7543613e5f62d12f65806536e3f99b19fe3b0e8
Ajhaa <atte.h.1998@gmail.com>
2019-04-11 19:06:11 +0300

    Autohighscores

--- app/core/src/com/mygdx/game/Highscores.java
+++ app/core/src/com/mygdx/game/Highscores.java
@@ -1,5 +1,6 @@
 package com.mygdx.game;
 
+import java.io.File;
 import java.io.IOException;
 
 import com.mygdx.game.io.FileIO;
@@ -7,10 +8,21 @@ import com.mygdx.game.io.FileIO;
 public class Highscores {
 
     public static String[] getScores() {
+        createHighScores();
         try {
             return FileIO.fileToArray("highscores.txt");
         } catch (IOException e) {
             return new String[]{"-"};
+        }
+    }
+
+    private static void createHighScores() {
+        if (!new File("highscores.txt").exists()) {
+            try {
+                FileIO.createFile("highscores.txt", new String[]{});
+            } catch (Exception e) {
+                System.out.println("Config creation failed: " + e.getMessage());
+            }
         }
     }
 }
