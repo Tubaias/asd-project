ba2ba9db7e79d103b6d20102516c20fdac8c354f
Topias Teittinen <topias_t@hotmail.com>
2019-05-07 17:54:20 +0300

    thingeroos

--- app/core/src/com/mygdx/game/utility/logic/BulletFactory.java
+++ app/core/src/com/mygdx/game/utility/logic/BulletFactory.java
@@ -33,7 +33,7 @@ public class BulletFactory {
         largePlayerBulletTexture = new Texture("images/bullets/largeplayerbullet.png");
         angledBulletTexture = new Texture("images/bullets/enemybullet.png");
         missileTexture = new Texture("images/bullets/missile.png");
-        specialBulletTexture = new Texture("images/bullets/star.png");
+        specialBulletTexture = new Texture("images/bullets/fidgetSpinner.png");
     }
 
     public Bullet createBullet(BulletType type, float x, float y, float angle) {
